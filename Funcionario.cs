31fcf32fda3de2d244a599630579c5c4ca654d42
marcelochirai <65429412+marcelochirai@users.noreply.github.com>
2020-09-30 23:55:05 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double Liquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + ", R$ "
+            + Liquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func=new Funcionario();
+
+            Console.WriteLine("Informe o nome do funcionário: ");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Qual o salário bruto? ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual o valor do imposto? ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(); 
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine("Em quantos % deseja aumentar o salário? "); 
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            func.AumSalario(porcent);
+
+            Console.WriteLine(); 
+            Console.WriteLine("Dados atualizados: " + func);
+
+        }
+    }
+}
